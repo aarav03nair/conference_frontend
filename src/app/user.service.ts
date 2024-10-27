ba3675c664b1aa8f188abc7ca4feaaa223a67d06
aarav03nair <91147978+aarav03nair@users.noreply.github.com>
2024-10-27 12:37:57 +0530

    Update user.service.ts with render link backend

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
 
-  private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = 'https://conference-backend-3nta.onrender.com';
 
   constructor(private http: HttpClient) {}
 
